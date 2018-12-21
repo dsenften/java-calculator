42abf5246117d3c0ee5567c2dd6f752a5b534f03
Daniel Senften <daniel@senften.org>
2018-12-21 12:26:22 +0100

    Projektgerüst definiert.

---
+++ src/main/java/gibb/Calculator.java
@@ -0,0 +1,18 @@
+package gibb;
+
+/**
+ * Dies ist das initiale Gerüst für unseren Rechner. Das Programm selber
+ * soll eine Rechnug auf der Kommandozeile verarbeiten, in der Form:
+ *
+ * <code>
+ *     $ gibb.Calculator 123.4 + 56.7
+ *     180.1
+ *     
+ * </code>
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        // TODO your code goes here
+    }
+}
